fb7a530f1ffb760f51a705652c5522ad236259ce
Javascript743 <114839381+Javascript743@users.noreply.github.com>
2023-11-20 20:10:03 +0900

    Create AnisotropicFiltering.cs

---
+++ Unityengine/AnisotropicFiltering.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace UnityEngine
+{
+	// Token: 0x02000154 RID: 340
+	public enum AnisotropicFiltering
+	{
+		// Token: 0x04000423 RID: 1059
+		Disable,
+		// Token: 0x04000424 RID: 1060
+		Enable,
+		// Token: 0x04000425 RID: 1061
+		ForceEnable
+	}
+}
